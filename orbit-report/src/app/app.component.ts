ae5bedd88f3aa28615ded04c122ac4678d134ac2
aniorill <x.iorillo@gmail.com>
2020-06-01 17:46:25 -0400

    Clickable sort, Warning colors, initial search bar

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -12,35 +12,50 @@ export class AppComponent {
 
   constructor() {
     this.sourceList = [
-       new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
-       new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
-       new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
-       new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
-       new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true),
+      new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
+      new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
+      new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
+      new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
+      new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true),
     ];
     const satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function(response) {
       response.json().then(function(data) {
 
-         const fetchedSatellites = data.satellites;
-         // TODO: loop over satellites
-         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-         let satellite: object;
-         // TODO: loop over satellites
-         for (const data of fetchedSatellites) {
-           satellite = new Satellite(
-             data.name,
-             data.type,
-             data.launchDate,
-             data.orbitType,
-             data.operational
-           );
-           this.sourceList.push(satellite);
-         }
+        const fetchedSatellites = data.satellites;
+        // TODO: loop over satellites
+        // TODO: create a Satellite object using new
+        // Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type,
+        // fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+        let satellite: object;
+        // TODO: loop over satellites
+        for (const data of fetchedSatellites) {
+          satellite = new Satellite(
+            data.name,
+            data.type,
+            data.launchDate,
+            data.orbitType,
+            data.operational
+          );
+          this.sourceList.push(satellite);
+        }
 
       }.bind(this));
-   }.bind(this));
- }
+    }.bind(this));
+  }
+  search(searchTerm: string): void {
+    const matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for (const datum of matchingSatellites) {
+      const name = this.sourceList.name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList);
+      }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+  }
 }
